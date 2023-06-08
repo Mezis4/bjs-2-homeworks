167bf46101fe5db616d67d8163b9175c8e69a9a7
mezitest <mezis4u@inbox.ru>
2023-06-08 20:26:19 +0600

    Complete HomeWork 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
-function compareArrays(arr1, arr2) {
-  
-}
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const arr = [...users];
+  let result = arr.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, array) => {
+    acc += item;
+    if(index === array.length - 1) {
+      return acc / array.length;
+    }
+    return acc;
+  }, 0).toFixed(1);
+  return parseFloat(result);
 }
