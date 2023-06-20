0e8a907ca6f58fa71cec6654f432d18f18b07486
mezitest <mezis4u@inbox.ru>
2023-06-20 22:36:36 +0600

    first try

---
+++ 7.async/task.js
@@ -0,0 +1,59 @@
+class AlarmClock {
+  constructr() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || callback === undefined) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    if (this.alarmCollection.some((alarm) => alarm.time === time)) {
+      console.warn('Уже присутствует звонок на это же время');
+    }
+
+    this.alarmCollection.push({ time, callback, canCall: true });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    const d = new Date();
+    let currentDate = d.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return currentDate;
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() =>
+      this.alarmCollection.forEach((alarms) => {
+        if (alarms.time === this.getCurrentFormattedTime() && alarms.canCall) {
+          alarms.canCall = false;
+          alarms.callback();
+        }
+      }), 2000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach((calls) => calls.canCall = true)
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
